4983c2b1d9ee8c54cffa847847360868d77ad42b
Bryan Powell <bryan@metabahn.com>
2020-10-12 10:10:09 -0700

    Push to the repository full name

--- lib/commit/operations/git/push.rb
+++ lib/commit/operations/git/push.rb
@@ -6,7 +6,7 @@ module Commit
   module Operations
     module Git
       class Push < Operation
-        def call(user: ENV["COMMIT__GIT_USER"], token: ENV["COMMIT__GIT_TOKEN"], repo: @event.config.dig("repository", "name"))
+        def call(user: ENV["COMMIT__GIT_USER"], token: ENV["COMMIT__GIT_TOKEN"], repo: @event.config.dig("repository", "full_name"))
           `git pull --rebase`
 
           `git push https://#{user}:#{token}@github.com/#{repo}.git`
