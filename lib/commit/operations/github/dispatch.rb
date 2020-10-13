c03e262ed766f5f59614286813316ec2c89f4bb7
Bryan Powell <bryan@metabahn.com>
2020-10-13 08:46:56 -0700

    Dispatch with the authorization header

--- lib/commit/operations/github/dispatch.rb
+++ lib/commit/operations/github/dispatch.rb
@@ -6,13 +6,14 @@ module Commit
   module Operations
     module Github
       class Dispatch < Operation
-        def call(repo:, user: ENV["COMMIT__GIT_USER"], token: ENV["COMMIT__GIT_TOKEN"])
+        def call(repo:, token: ENV["COMMIT__GIT_TOKEN"])
           command = <<~COMMAND
             curl \
               -v \
               -X POST \
+              -H "Authorization: token #{token}" \
               -H "Accept: application/vnd.github.v3+json" \
-              https://#{user}:#{token}@api.github.com/repos/#{repo}/dispatches \
+              https://api.github.com/repos/#{repo}/dispatches \
               -d '{"event_type":"commit.touch"}'
           COMMAND
 
