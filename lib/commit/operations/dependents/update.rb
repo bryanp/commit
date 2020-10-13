f1a11fcf05d48a7af5ebcda798359f7b3eb4c995
Bryan Powell <bryan@metabahn.com>
2020-10-13 08:32:43 -0700

    Add the update dependents tool

---
+++ lib/commit/operations/dependents/update.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require_relative "../../operation"
+require_relative "../github/dispatch"
+
+module Commit
+  module Operations
+    module Dependents
+      class Update < Operation
+        def call(user: ENV["COMMIT__GIT_USER"], token: ENV["COMMIT__GIT_TOKEN"])
+          each_dependent_config do |dependent_config|
+            Commit::Operations::Dependents::Update.call(scope: scope, event: event, repo: dependent_config["repo"])
+          end
+        end
+
+        # @api private
+        private def each_dependent_config
+          return enum_for(:each_dependent_config) unless block_given?
+
+          config["dependents"].to_a.each do |dependent_config|
+            yield dependent_config
+          end
+        end
+      end
+    end
+  end
+end
---
+++ lib/commit/operations/github/dispatch.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require_relative "../../operation"
+
+module Commit
+  module Operations
+    module Github
+      class Dispatch < Operation
+        def call(repo:, user: ENV["COMMIT__GIT_USER"], token: ENV["COMMIT__GIT_TOKEN"])
+          command = <<~COMMAND
+            curl \
+              -X POST \
+              -H "Accept: application/vnd.github.v3+json" \
+              https://#{user}:#{token}@api.github.com/repos/#{repo}/dispatches \
+              -d '{"event_type":"commit.touch"}'
+          COMMAND
+
+          `#{command}`
+        end
+      end
+    end
+  end
+end
