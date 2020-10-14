fb537ba797dfcd89228496d1e986f13088226535
Bryan Powell <bryan@metabahn.com>
2020-10-14 13:06:45 -0700

    Introduce operation artifacts, with cleanup

--- lib/commit/operation.rb
+++ lib/commit/operation.rb
@@ -9,7 +9,7 @@ module Commit
   # Operations perform one or more actions on a scope.
   #
   class Operation
-    attr_reader :scope, :event
+    attr_reader :scope, :event, :artifacts
 
     extend Forwardable
     def_delegators :"@scope", :config
@@ -17,17 +17,28 @@ module Commit
     def initialize(scope:, event:)
       @scope = scope
       @event = event
+      @artifacts = []
     end
 
     def call(*args, **kwargs)
       # implemented by subclasses
     end
 
+    # @api private
+    private def cleanup
+      @artifacts.each do |path|
+        FileUtils.rm_r(path)
+      end
+    end
+
     class << self
       def call(*args, scope:, event:, **kwargs)
         instance = new(scope: scope, event: event)
         instance.call(*args, **kwargs)
+        yield instance if block_given?
         instance
+      ensure
+        instance.send(:cleanup)
       end
     end
   end
